c927a908b03c77eeb76cdc76a98e42c27a757e4d
ryanyuri357 <ryan.yuri357@gmail.com>
2021-08-10 00:25:10 -0500

    next work on update contact

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,5 +1,3 @@
-//TODO: set NavbarBrand to go to home page and export Header
-
 import React from "react";
 import { Navbar, NavbarBrand, NavbarText } from "reactstrap";
 import { Link } from "react-router-dom";
@@ -7,10 +5,14 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <Navbar color="info" light>
-      <NavbarBrand className="text-white">LCO Contact App</NavbarBrand>
+      <NavbarBrand tag={Link} to="/" className="text-white">
+        RYP Contact App
+      </NavbarBrand>
       <NavbarText className="text-white float-right">
         A simple Contact app
       </NavbarText>
     </Navbar>
   );
 };
+
+export default Header;
--- src/pages/AddContact.js
+++ src/pages/AddContact.js
@@ -13,20 +13,20 @@ import {
   Button,
   Spinner,
   Row,
-  Col
+  Col,
 } from "reactstrap";
 
 // to compress image before uploading to the server
 import { readAndCompressImage } from "browser-image-resizer";
 
 // configs for image resizing
-//TODO: add image configurations
+import { imageConfig } from "../utils/config";
 
 import { MdAddCircleOutline } from "react-icons/md";
 
 import { v4 } from "uuid";
 
-// context stuffs
+// context stuff
 import { ContactContext } from "../context/Context";
 import { CONTACT_TO_UPDATE } from "../context/action.types";
 
@@ -71,23 +71,109 @@ const AddContact = () => {
   }, [contactToUpdate]);
 
   // To upload image to firebase and then set the the image link in the state of the app
-  const imagePicker = async e => {
-    // TODO: upload image and set D-URL to state
+  const imagePicker = async (e) => {
+    // TODO: Upload image and set D-URL to state
+
+    try {
+      const file = e.target.files[0];
+
+      var metadata = {
+        contentType: file.type,
+      };
+
+      let resizedImage = await readAndCompressImage(file, imageConfig);
+
+      const storageRef = await firebase.storage().ref();
+      var uploadTask = storageRef
+        .child("images/" + file.name)
+        .put(resizedImage, metadata);
+
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot) => {
+          setIsUploading(true);
+          var progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+          switch (snapshot.state) {
+            case firebase.storage.TaskState.PAUSED:
+              setIsUploading(false);
+              console.log("Uploading is paused");
+              break;
+            case firebase.storage.TaskState.RUNNING:
+              console.log("Uploading is progress...");
+              break;
+          }
+          if (progress == 100) {
+            setIsUploading(false);
+            toast("Uploaded", { type: "success" });
+          }
+        },
+        (error) => {
+          toast("something is wrong in state change", { type: "error" });
+        },
+        () => {
+          uploadTask.snapshot.ref
+            .getDownloadURL()
+            .then((downloadURL) => {
+              setDownloadUrl(downloadURL);
+            })
+            .catch((err) => console.log(err));
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      toast("Something went wrong", { type: error });
+    }
   };
 
   // setting contact to firebase DB
   const addContact = async () => {
     //TODO: add contact method
+    try {
+      firebase
+        .database()
+        .ref("contacts/" + v4())
+        .set({
+          name,
+          email,
+          phoneNumber,
+          address,
+          picture: downloadUrl,
+          star,
+        });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   // to handle update the contact when there is contact in state and the user had came from clicking the contact update icon
   const updateContact = async () => {
     //TODO: update contact method
+    try {
+      firebase
+        .database()
+        .ref("contacts/" + contactToUpdateKey)
+        .set({
+          name,
+          email,
+          phoneNumber,
+          address,
+          picture: downloadUrl,
+          star,
+        });
+    } catch (error) {
+      console.log(error);
+      toast("Opps..", { type: "error" });
+    }
   };
 
   // firing when the user click on submit button or the form has been submitted
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+    isUpdate ? updateContact() : addContact();
+
+    toast("Success", { type: "success" });
 
     // isUpdate wll be true when the user came to update the contact
     // when their is contact then updating and when no contact to update then adding contact
@@ -97,7 +183,7 @@ const AddContact = () => {
     dispatch({
       type: CONTACT_TO_UPDATE,
       payload: null,
-      key: null
+      key: null,
     });
 
     // after adding/updating contact then sending to the contacts
@@ -126,7 +212,7 @@ const AddContact = () => {
                     id="imagepicker"
                     accept="image/*"
                     multiple={false}
-                    onChange={e => imagePicker(e)}
+                    onChange={(e) => imagePicker(e)}
                     className="hidden"
                   />
                 </div>
@@ -140,7 +226,7 @@ const AddContact = () => {
                 id="name"
                 placeholder="Name"
                 value={name}
-                onChange={e => setName(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
               />
             </FormGroup>
             <FormGroup>
@@ -149,7 +235,7 @@ const AddContact = () => {
                 name="email"
                 id="email"
                 value={email}
-                onChange={e => setEmail(e.target.value)}
+                onChange={(e) => setEmail(e.target.value)}
                 placeholder="Email"
               />
             </FormGroup>
@@ -159,7 +245,7 @@ const AddContact = () => {
                 name="number"
                 id="phonenumber"
                 value={phoneNumber}
-                onChange={e => setPhoneNumber(e.target.value)}
+                onChange={(e) => setPhoneNumber(e.target.value)}
                 placeholder="phone number"
               />
             </FormGroup>
@@ -169,7 +255,7 @@ const AddContact = () => {
                 name="area"
                 id="area"
                 value={address}
-                onChange={e => setAddress(e.target.value)}
+                onChange={(e) => setAddress(e.target.value)}
                 placeholder="address"
               />
             </FormGroup>
